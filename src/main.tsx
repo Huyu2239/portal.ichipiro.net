65df47ea42b0e6abb4c6252ccd0f412d239aafcc
Huyu2239 <huyu2239@gmail.com>
2024-10-19 12:34:05 +0900

    add: IndexPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { IndexPage } from "./pages/index.tsx";
 import "./styles/theme.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("No root element");
 
-createRoot(root).render(<StrictMode />);
+const router = createBrowserRouter([
+	{
+		children: [
+			{
+				index: true,
+				element: <IndexPage />,
+			},
+		],
+	},
+]);
+
+createRoot(root).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
